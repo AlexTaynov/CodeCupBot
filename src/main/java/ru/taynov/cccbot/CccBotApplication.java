4a2c53160710ace810be57fed29ba6fdd24c9599
a.taynov <a.taynov@tinkoff.ru>
2023-09-12 18:40:46 +0300

    init commit

---
+++ src/main/java/ru/taynov/cccbot/CccBotApplication.java
@@ -0,0 +1,13 @@
+package ru.taynov.cccbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CccBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/taynov/cccbot/CccBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taynov.cccbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
